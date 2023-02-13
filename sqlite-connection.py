0c93d25bbf7e6e8e5999fb209d58a4e209b64bc1
Maciej Polcyn <maciej.polcynn@gmail.com>
2023-02-14 00:21:33 +0100

    Adds creating tables functions to the sqlite connection file

--- sqlite-connection.py
+++ sqlite-connection.py
@@ -24,3 +24,42 @@ def execute_query(connection, query):
 
 connection = create_connection("C:/Users/Kamilka/PycharmProjects/sqlite-databases/sm_app.sqlite")
 
+create_users_table = """
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  age INTEGER,
+  gender TEXT,
+  nationality TEXT
+);
+"""
+create_posts_table = """
+CREATE TABLE IF NOT EXISTS posts(
+  id INTEGER PRIMARY KEY AUTOINCREMENT, 
+  title TEXT NOT NULL, 
+  description TEXT NOT NULL, 
+  user_id INTEGER NOT NULL, 
+  FOREIGN KEY (user_id) REFERENCES users (id)
+);
+"""
+create_comments_table = """
+CREATE TABLE IF NOT EXISTS comments (
+  id INTEGER PRIMARY KEY AUTOINCREMENT, 
+  text TEXT NOT NULL, 
+  user_id INTEGER NOT NULL, 
+  post_id INTEGER NOT NULL, 
+  FOREIGN KEY (user_id) REFERENCES users (id) FOREIGN KEY (post_id) REFERENCES posts (id)
+);
+"""
+create_likes_table = """
+CREATE TABLE IF NOT EXISTS likes (
+  id INTEGER PRIMARY KEY AUTOINCREMENT, 
+  user_id INTEGER NOT NULL, 
+  post_id integer NOT NULL, 
+  FOREIGN KEY (user_id) REFERENCES users (id) FOREIGN KEY (post_id) REFERENCES posts (id)
+);
+"""
+execute_query(connection, create_users_table)
+execute_query(connection, create_posts_table)
+execute_query(connection, create_comments_table)
+execute_query(connection, create_likes_table)
