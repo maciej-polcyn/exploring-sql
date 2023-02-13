ed9977c71bb9c7ea0189663989620f0a14834115
Maciej Polcyn <maciej.polcynn@gmail.com>
2023-02-14 00:19:47 +0100

    Adds a sqlite connection file

---
+++ sqlite-connection.py
@@ -0,0 +1,26 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(path):
+    connection = None
+    try:
+        connection = sqlite3.connect(path)
+        print("Connection to SQLite DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+    return connection
+
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+
+connection = create_connection("C:/Users/Kamilka/PycharmProjects/sqlite-databases/sm_app.sqlite")
+
